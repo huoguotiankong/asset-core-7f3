6c99d6ba2795cfd59fc831e4688532e9ee7f8b75
huoguotiankong <76257388+huoguotiankong@users.noreply.github.com>
2026-08-22 17:49:28 +0800

    hanime1: add Test27 recovery loader

---
+++ apps/video/hanime1/releases/2.0.0-test.27/recovery_loader.js
@@ -0,0 +1,9 @@
+/* Hanime1 Test27 recovery: Test26 creator base + reply recovery + icon UX */
+(function(){
+var ROOT='https://raw.githubusercontent.com/huoguotiankong/asset-core-7f3/main/';
+require(ROOT+'apps/video/hanime1/releases/2.0.0-test.26/recovery_loader.js?hanime_recovery=27',{headers:{'Cache-Control':'no-cache'}},20027);
+require(ROOT+'apps/video/hanime1/releases/2.0.0-test.27/patch_experience27.js?hanime_experience=27',{headers:{'Cache-Control':'no-cache'}},20027);
+HanimePages.build='2.0.0-test.27';
+HanimeCore.build='2.0.0-test.27';
+HanimeProvider.build='2.0.0-test.27';
+})();
