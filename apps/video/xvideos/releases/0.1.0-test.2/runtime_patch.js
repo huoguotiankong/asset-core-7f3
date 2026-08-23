49bf6abe8b00cbef6bd749e837bac8de673b6aec
huoguotiankong <76257388+huoguotiankong@users.noreply.github.com>
2026-08-23 19:06:36 +0800

    xvideos: bump runtime to Test2

---
+++ apps/video/xvideos/releases/0.1.0-test.2/runtime_patch.js
@@ -0,0 +1,6 @@
+/* XVideos Runtime Patch 0.1.0-test.2 */
+(function(){
+  if(typeof XVideosRemoteRuntime==='undefined')throw new Error('XVideosRemoteRuntime missing before Test2 patch');
+  XVideosRemoteRuntime.version='0.1.0-test.2';
+  XVideosRemoteRuntime.build=10102;
+})();
