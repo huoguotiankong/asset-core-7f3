0008aaa9078ccf13e2ea1a604095d6571a6c9731
huoguotiankong <76257388+huoguotiankong@users.noreply.github.com>
2026-08-23 12:32:10 +0800

    myav: add Test7 neutral shell version patch

---
+++ apps/video/myav/releases/0.1.0-test.7/version_patch.js
@@ -0,0 +1,7 @@
+/* MyAv 0.1.0-test.7 - neutral import shell/version patch */
+(function(R){
+  if(!R)throw new Error('MyAvRemoteRuntime missing for Test7');
+  R.version='0.1.0-test.7';
+  R.build=10107;
+  R.bootstrapUrl='https://raw.githubusercontent.com/huoguotiankong/asset-core-7f3/main/apps/video/myav/bootstrap_test_v7_b10107.js';
+})(MyAvRemoteRuntime);
