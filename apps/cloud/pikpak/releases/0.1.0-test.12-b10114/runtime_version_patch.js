ce43cf7fb2fd89203013c91f76cdca5a8d2fb6db
huoguotiankong <76257388+huoguotiankong@users.noreply.github.com>
2026-09-24 00:36:28 +0800

    pikpak: mark Test12 runtime build

---
+++ apps/cloud/pikpak/releases/0.1.0-test.12-b10114/runtime_version_patch.js
@@ -0,0 +1,6 @@
+/* PikPak Test12 Build10114 - runtime identity */
+(function(R){
+    var baseModule=R.module;
+    R.module=function(){var m=baseModule();m.version='0.1.0-test.12';m.build=10114;return m;};
+    R.version='0.1.0-test.12';R.build=10114;
+})(PikPakRemoteRuntime);
