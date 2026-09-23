fcfc38aeb784bf082ccb9f4bba41429782a9485f
huoguotiankong <76257388+huoguotiankong@users.noreply.github.com>
2026-09-23 22:35:02 +0800

    Add PikPak Test9 immutable installer

---
+++ cloud/pikpak/v0.1.0-test.9/import_tx_captcha_fix.js
@@ -0,0 +1,19 @@
+(function(){
+    var old=null,oldIcon='';
+    try{
+        var raw=String(request('hiker://home@PikPak')||'');
+        var p=raw.indexOf('￥home_rule￥');
+        if(p>=0)raw=raw.substring(p+'￥home_rule￥'.length);else{p=raw.indexOf('{');if(p>0)raw=raw.substring(p);}
+        old=JSON.parse(raw);oldIcon=String(old.icon||'');
+    }catch(e){}
+    try{clearItem('password');clearItem('password_bak');}catch(e2){}
+    try{
+        clearMyVar('pikpak_v2_verify_url');clearMyVar('pikpak_v2_verify_token');clearMyVar('pikpak_v2_verified_captcha');clearMyVar('pikpak_v2_verify_handoff_done');clearMyVar('pikpak_v2_login_pass');
+    }catch(e3){}
+    var shell=String(request('https://raw.githubusercontent.com/huoguotiankong/asset-core-7f3/2298a6457e731c05e42f647a155999fc8274b809/apps/cloud/pikpak/pikpak_remote_test_v9_b10109.txt')||'');
+    var mark='￥home_rule￥',idx=shell.indexOf(mark);
+    if(idx<0)return shell;
+    var prefix=shell.substring(0,idx+mark.length),body=shell.substring(idx+mark.length),next=JSON.parse(body);
+    if(oldIcon)next.icon=oldIcon;
+    return prefix+JSON.stringify(next);
+})()
