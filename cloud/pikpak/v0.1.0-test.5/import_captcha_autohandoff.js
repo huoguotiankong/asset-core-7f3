44f36e8f2cf92b42863bd22da8657db1010fd01c
huoguotiankong <76257388+huoguotiankong@users.noreply.github.com>
2026-09-23 22:14:33 +0800

    Add PikPak Test5 import installer

---
+++ cloud/pikpak/v0.1.0-test.5/import_captcha_autohandoff.js
@@ -0,0 +1,25 @@
+(function(){
+    var old=null,oldIcon='';
+    try{
+        var raw=String(request('hiker://home@PikPak')||'');
+        var p=raw.indexOf('￥home_rule￥');
+        if(p>=0)raw=raw.substring(p+'￥home_rule￥'.length);else{p=raw.indexOf('{');if(p>0)raw=raw.substring(p);}
+        old=JSON.parse(raw);
+        oldIcon=String(old.icon||'');
+    }catch(e){}
+    try{clearItem('password');clearItem('password_bak');}catch(e2){}
+    try{
+        clearMyVar('pikpak_v2_login_pass');
+        clearMyVar('pikpak_v2_verify_token');
+        clearMyVar('pikpak_v2_verify_url');
+        clearMyVar('pikpak_v2_verify_user');
+        clearMyVar('pikpak_v2_verified_captcha');
+        clearMyVar('pikpak_v2_verify_handoff_done');
+    }catch(e3){}
+    var shell=String(request('https://raw.githubusercontent.com/huoguotiankong/asset-core-7f3/5c020787e09f3acc6be0d602c05b33af48fc8738/apps/cloud/pikpak/pikpak_remote_test_v5_b10105.txt')||'');
+    var mark='￥home_rule￥',idx=shell.indexOf(mark);
+    if(idx<0)return shell;
+    var prefix=shell.substring(0,idx+mark.length),body=shell.substring(idx+mark.length),next=JSON.parse(body);
+    if(oldIcon)next.icon=oldIcon;
+    return prefix+JSON.stringify(next);
+})()
