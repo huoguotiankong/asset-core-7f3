750f332741cba6fc78c6f7561064c4ddb48c221c
huoguotiankong <76257388+huoguotiankong@users.noreply.github.com>
2026-09-24 13:20:37 +0800

    xchina: correct Test9 category route before activation

---
+++ apps/video/xchina/releases/0.1.0-test.9/category_fix.js
@@ -0,0 +1,8 @@
+/* Test9 pre-activation correction: keep uploaded reading-source route exact. */
+(function(C){
+  try{
+    var a=C&&C.video&&C.video['情色电影'],i;
+    if(!a)return;
+    for(i=0;i<a.length;i++)if(a[i]&&a[i][0]==='欧美电影')a[i][1]='/videos/series-63964959ddb1b/{page}.html';
+  }catch(e){}
+})(XChinaTest9Categories);
